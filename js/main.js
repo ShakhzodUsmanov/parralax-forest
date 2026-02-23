c3526e9f0423be908b06df6db449775ae2074edf
Shakhzod <shaxzodusmanov11@gmail.com>
2026-02-23 15:10:28 +0500

    mouse positon

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("mousemove", e =>{
+  Object.assign(document.documentElement, {
+    style: `
+    --mouse-x: ${(e.clientX - window.innerWidth/ 2) * -0.05}deg; 
+    --mouse-y: ${(e.clientY - window.innerHeight/ 2) * -0.1}deg;
+    `
+  })
+})
